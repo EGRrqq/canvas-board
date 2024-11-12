6607987a2b722c1620bc892a56887ff9e87970e6
EGRrqq <egrrqqdev@gmail.com>
2024-11-12 10:23:12 -0500

    chore: rename build folder to dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	root: "src",
 	build: {
-		outDir: "../build",
+		outDir: "../dist",
 		assetsDir: "",
 	},
 	base: "/canvas-board/",
