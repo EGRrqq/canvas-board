deaa3c900e70b6b22dae4a50e4054dce8f5c12f9
EGRrqq <egrrqqdev@gmail.com>
2024-11-13 23:41:49 -0500

    clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,1 @@
-import { Graph, type IGraphData, type IGraphSettings } from "@/aStarAlgorithm";
 
-const graphData: IGraphData = {
-	width: 5,
-	height: 7,
-	obstacles: [
-		{ x: 1, y: 1 },
-		{ x: 2, y: 1 },
-	],
-	start: { x: 1, y: 3 },
-	end: { x: 4, y: 5 },
-};
-const graphSettings: IGraphSettings = { log: true };
-
-const path = await Graph.calcPath(graphData, graphSettings);
-console.log(path);
