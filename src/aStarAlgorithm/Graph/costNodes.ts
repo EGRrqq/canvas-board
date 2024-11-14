7a6851ab860eb1bdc1bc8afd8359759da1bdd149
EGRrqq <egrrqqdev@gmail.com>
2024-11-13 21:27:17 -0500

    refactor(aStart): consistency in naming props
    rename:
    - start -> startNode
    - end -> endNode

--- src/aStarAlgorithm/Graph/costNodes.ts
+++ src/aStarAlgorithm/Graph/costNodes.ts
@@ -2,17 +2,19 @@ import type { TGraph, TGraphNode } from "@/aStarAlgorithm/models";
 
 export const costNodes = async (
 	graph: TGraph,
-	start: TGraphNode,
-	end: TGraphNode,
+	startNode: TGraphNode,
+	endNode: TGraphNode,
 ): Promise<void> => {
 	for (const row of graph) {
 		for (const node of row) {
 			if (node.traversable) {
 				// gCost: расстояние от стартового узла
-				node.gCost = Math.abs(node.x - start.x) + Math.abs(node.y - start.y);
+				node.gCost =
+					Math.abs(node.x - startNode.x) + Math.abs(node.y - startNode.y);
 
 				// hCost: эвристическая стоимость до конечного узла (используем манхэттенское расстояние)
-				node.hCost = Math.abs(node.x - end.x) + Math.abs(node.y - end.y);
+				node.hCost =
+					Math.abs(node.x - endNode.x) + Math.abs(node.y - endNode.y);
 
 				// fCost: сумма gCost и hCost
 				node.fCost = node.gCost + node.hCost;
