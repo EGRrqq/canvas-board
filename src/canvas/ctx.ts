ce2de9a5ddbd35e72289607de8bc654be34a5271
EGRrqq <egrrqqdev@gmail.com>
2024-11-15 00:11:35 -0500

    refactor(canvas): add types for Ctx

--- src/canvas/ctx.ts
+++ src/canvas/ctx.ts
@@ -1,18 +1,27 @@
 let CTX: CanvasRenderingContext2D | null = null;
 
-const setCtx = (canvasId: string): void => {
-	const canvas = document.getElementById(canvasId);
+type TGetCtx = () => CanvasRenderingContext2D;
+type TSetCtx = (id: string) => { getCtx: TGetCtx };
+interface ICtx {
+	setCtx: TSetCtx;
+	getCtx: TGetCtx;
+}
+
+const setCtx: TSetCtx = (id) => {
+	const canvas = document.getElementById(id);
 
 	if (!(canvas instanceof HTMLCanvasElement))
-		throw new Error(`canvas елемент c id: '${canvasId}' не найден`);
+		throw new Error(`canvas елемент c id: '${id}' не найден`);
 
 	CTX = canvas.getContext("2d");
+
+	return { getCtx };
 };
 
-const getCtx = (): CanvasRenderingContext2D => {
+const getCtx: TGetCtx = () => {
 	if (!CTX) throw new Error("Ошибка во время получения 2д контекста");
 
 	return CTX;
 };
 
-export const Ctx = { setCtx, getCtx };
+export const Ctx: ICtx = { setCtx, getCtx };
