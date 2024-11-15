e988f12e9c829ce5c8bb11ca58293565986f39e1
EGRrqq <egrrqqdev@gmail.com>
2024-11-14 23:30:04 -0500

    feat(canvas): draw rectangle
    feat(canvas): structure init

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import "style.css";
+
+import { Canvas } from "@/canvas";
+import type { Rect } from "./models";
+
+const rect1: Rect = {
+	position: { x: 100, y: 100 },
+	size: { width: 100, height: 50 },
+};
+
+const rect2: Rect = {
+	position: { x: 150, y: 50 },
+	size: { width: 50, height: 50 },
+};
+
+Canvas("board").drawRect(rect1).drawRect(rect2);
