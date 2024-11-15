01ff9b92804d25cd32cb44499109d09dd4b50ff8
EGRrqq <egrrqqdev@gmail.com>
2024-11-15 04:39:30 -0500

    feat(canvas): remove Render from settings e8aa801
    cause this setting is useless for our application, keep it simple

--- src/canvas/index.ts
+++ src/canvas/index.ts
@@ -2,21 +2,15 @@ import * as Ctx from "@/canvas/ctx";
 import { type IDraw, drawRect } from "@/canvas/draw";
 import * as Render from "@/canvas/render";
 
-type ICanvasSettings = {
-	scale: boolean;
-};
+type ICanvasSettings = {};
 type TCanvas = (id: string, settings?: Partial<ICanvasSettings>) => IDraw;
 
-const defaultSettings: ICanvasSettings = {
-	scale: true,
-};
+const defaultSettings: ICanvasSettings = {};
 
 export const Canvas: TCanvas = (id, settings = defaultSettings) => {
 	// Необходимо для инициализации
 	Ctx.setCtx(id);
-
-	// Настройки
-	settings.scale ?? Render.clear().scale();
+	Render.clear().scale();
 
 	return { drawRect };
 };
