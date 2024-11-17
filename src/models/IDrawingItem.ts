d99332fa28f6e55f8d0731e1ee37b3e94e562eff
EGRrqq <egrrqqdev@gmail.com>
2024-11-17 05:18:13 -0500

    feat(ui): drawingItem interface design

---
+++ src/models/IDrawingItem.ts
@@ -0,0 +1,22 @@
+import type { IDrawRectSettings } from "@/canvas";
+import type { IDrawPathSettings } from "@/canvas/draw";
+import type { Point, Size } from "@/models";
+
+type IToolSettingsMap = {
+	rect: IDrawRectSettings;
+	line: IDrawPathSettings;
+};
+type TToolType = keyof IToolSettingsMap;
+
+interface ITool<TT extends TToolType> {
+	type: TT;
+	settings: IToolSettingsMap[TT];
+}
+
+export interface IDrawingItem<TT extends TToolType> {
+	id: string;
+	tool: ITool<TT>;
+	boundElem: Pick<IDrawingItem<TT>, "id">[];
+	point: Point;
+	size: Size;
+}
